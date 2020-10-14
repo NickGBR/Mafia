f33bafb2107fd72d7d9b4929bb56970bcbce9cf4
Polina Bykova <bykova.spbu@gmail.com>
2020-10-14 13:27:54 +0300

    commit

--- src/main/java/org/dreamteam/mafia/dto/UserDTO.java
+++ src/main/java/org/dreamteam/mafia/dto/UserDTO.java
@@ -1,7 +1,12 @@
 package org.dreamteam.mafia.dto;
 
+import org.dreamteam.mafia.dao.Room;
+import org.dreamteam.mafia.dao.Statistics;
+
 /**
  * Пользователь, восстановленый из JSON, пришедшего со стороны клиента
  */
 public class UserDTO {
+
+
 }
