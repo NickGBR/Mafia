43302cb5936dcb1b733b02a07838eb972bbd2b52
Polina Bykova <bykova.spbu@gmail.com>
2020-10-15 15:00:57 +0300

    migrations fixed

--- src/main/java/org/dreamteam/mafia/Testing.java
+++ src/main/java/org/dreamteam/mafia/Testing.java
@@ -1,4 +1,4 @@
-package org.dreamteam.mafia.service.implementation;
+package org.dreamteam.mafia;
 
 import org.dreamteam.mafia.dao.UserDAO;
 import org.dreamteam.mafia.repository.api.CrudUserRepository;
@@ -9,30 +9,18 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-// @SpringBootApplication
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
-@ComponentScan("org.dreamteam.mafia")
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+// @ComponentScan("org.dreamteam.mafia")
 public class Testing {
-
-
-    public static void main(String[] args) {
-        SpringApplication.run(Testing.class);
-    }
 
     @Bean
     public CommandLineRunner demo(CrudUserRepository repository) {
         return (args) -> {
-            UserDAO user3 = repository.findByLogin("Poly").get(0);
-            System.out.println(user3);
-            Iterable<UserDAO> usersInDb = repository.findAll();
+            UserDAO user = repository.findByLogin("user1").get(0);
+            System.out.println(user);
 
-            repository.save(new UserDAO("123456789", "Poly2"));
-            // save a few customers
-          //  repository.save(new User(123, "Dessler"));
-
+            repository.save(new UserDAO("123456789", "Poly3"));
         };
-
-
     }
 
 }
