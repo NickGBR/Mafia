0e9e544f976920c4ad08c23fe681a0e7ebd23cf4
Polina Bykova <bykova.spbu@gmail.com>
2020-10-19 19:43:03 +0300

    room search by game state done

--- mafia-web/src/main/java/org/dreamteam/mafia/Testing.java
+++ mafia-web/src/main/java/org/dreamteam/mafia/Testing.java
@@ -3,6 +3,7 @@ package org.dreamteam.mafia;
 import org.dreamteam.mafia.dao.MessageDAO;
 import org.dreamteam.mafia.dao.RoomDAO;
 import org.dreamteam.mafia.dao.UserDAO;
+import org.dreamteam.mafia.dao.enums.GameStatusEnum;
 import org.dreamteam.mafia.repository.api.CrudUserRepository;
 import org.dreamteam.mafia.repository.api.MessageRepository;
 import org.dreamteam.mafia.repository.api.RoomRepository;
@@ -11,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Testing {
 
@@ -18,11 +21,10 @@ public class Testing {
     public CommandLineRunner demo(RoomRepository repository) {
         return (args) -> {
 
-            Iterable<RoomDAO> all = repository.findAll();
-            for (RoomDAO r : all) {
+            List<RoomDAO> rooms = repository.findRoomDAOByGameStatus(GameStatusEnum.DELETED);
+            for (RoomDAO r : rooms) {
                 System.out.println(r);
             }
-
 
 
 
