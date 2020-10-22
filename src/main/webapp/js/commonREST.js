a11ee82d0b39ba40bc0d7354ca9b9dffff9a1441
Vasiliy Kuzmin <kuzymvas@yandex.ru>
2020-10-22 18:10:26 +0300

    ~ Проведен рефакторинг кода JS-клиента
    Signed-off-by: Vasiliy Kuzmin <kuzymvas@yandex.ru>

---
+++ src/main/webapp/js/commonREST.js
@@ -0,0 +1,122 @@
+function readCookie(name) {
+    let nameEQ = name + '=',
+        allCookies = document.cookie.split(';'),
+        i,
+        cookie;
+    for (i = 0; i < allCookies.length; i += 1) {
+        cookie = allCookies[i];
+        while (cookie.charAt(0) === ' ') {
+            cookie = cookie.substring(1, cookie.length);
+        }
+        if (cookie.indexOf(nameEQ) === 0) {
+            return cookie.substring(nameEQ.length, cookie.length);
+        }
+    }
+    return null;
+}
+
+function sendRequest(method, url, data, callback, allowedErrors) {
+    const token = readCookie("token");
+    console.log("Current token: " + token);
+    let request = new XMLHttpRequest();
+    request.open(method, url, true);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.setRequestHeader("Authorization", "Bearer " + token);
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (processResponse(request, allowedErrors)) {
+                callback(request);
+            }
+        }
+    };
+    request.send(data);
+}
+
+function processResponse(request, allowedCodes) {
+    if (isOkay(request)) {
+        return true;
+    }
+    if (isUnauthorised(request)) {
+        window.location.href = "login.html";
+        return false;
+    }
+    const errorMessage = processErrorResponse(request, allowedCodes);
+    alert(errorMessage);
+    return false;
+
+
+}
+
+function isOkay(request) {
+    return request.status === 200
+}
+
+function isUnauthorised(request) {
+    return request.status === 401
+}
+
+function processErrorResponse(request, allowedCodes) {
+    if (request.status === 400) {
+        const data = JSON.parse(request.responseText);
+        const code = parseInt(data["result"]);
+        if (code === -1) {
+            return "Ошибка логики клиента. Сервер не смог распознать отправленный запрос. Обратитесь к разработчику."
+        }
+        if (!allowedCodes.includes(code)) {
+            return "Ошибка внутренней логики. Сервер вернул код ошибки, недопустимый для данной операции. Обратитесь к разработчику.";
+        }
+        return processErrorCode(code);
+    } else if (request.status === 500) {
+        const data = JSON.parse(request.responseText);
+        switch (parseInt(data["result"])) {
+            case 1: {
+                return "Ошибка внутренней логики. Сервер не смог обработать запрос. Обратитесь к разработчику.";
+            }
+            case 2: {
+                return "Ошибка внутренней логики. База данных не смогла обработать запрос. Обратитесь к разработчику.";
+            }
+        }
+    }
+    return "Неизвестная ошибка. Сервер вернул код " + request.status;
+}
+
+function processErrorCode(code) {
+    switch (code) {
+        case 1: {
+            return "Пароль и его подтверждение не совпадают"
+        }
+        case 2: {
+            return "Такое имя пользователя уже используется"
+        }
+        case 3: {
+            return "Такого пользователя не существует"
+        }
+        case 4: {
+            return "Введен неверный пароль"
+        }
+        case 5: {
+            return "Данное действие недопустимо, когда вы уже находитесь в комнате"
+        }
+        case 6: {
+            return "Данное действие недопустимо в текущую игровую фазу"
+        }
+        case 7: {
+            return "Данное действие недопустимо, так как целовой пользователь не находится в вашей комнате"
+        }
+        case 8: {
+            return "Данное действие недопустимо, когда вы не находитесь в комнате"
+        }
+        case 9: {
+            return "Только администратор комнаты может выполнять данное действие"
+        }
+        case 10: {
+            return "Комната не существует"
+        }
+        case 11: {
+            return "Данное действие недопустимо для комнаты, игра в которой уже началась"
+        }
+        default: {
+            return "Ошибка внутренней логики. Сервер вернул код ошибки, неизвестный клиенту. Обратитесь к разработчику.";
+        }
+    }
+}
--- src/main/webapp/js/testRequests.js
+++ src/main/webapp/js/testRequests.js
@@ -1,342 +1,98 @@
-function readCookie(name) {
-    let nameEQ = name + '=',
-        allCookies = document.cookie.split(';'),
-        i,
-        cookie;
-    for (i = 0; i < allCookies.length; i += 1) {
-        cookie = allCookies[i];
-        while (cookie.charAt(0) === ' ') {
-            cookie = cookie.substring(1, cookie.length);
-        }
-        if (cookie.indexOf(nameEQ) === 0) {
-            return cookie.substring(nameEQ.length, cookie.length);
-        }
-    }
-    return null;
-}
-
 function getUserName() {
-    const token = readCookie("token");
-    console.log("Current token: " + token);
-    let request = new XMLHttpRequest();
-    request.open("GET", "/api/user/getCurrentName", true);
-    request.setRequestHeader("Content-Type", "application/json");
-    request.setRequestHeader("Authorization", "Bearer " + token);
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                let output = document.getElementById("output_box");
-                output.textContent = ''; // Убираем все элементы потомки, заменяя их на пустую строку
-                const textNode = document.createTextNode("Имя пользователя: " + request.responseText);
-                output.appendChild(textNode)
-            } else if (request.status === 401) {
-                console.log("Non authorised!");
-            } else if (request.status === 500) {
-                const data = JSON.parse(request.responseText);
-                switch (parseInt(data["result"])) {
-                    case 1: {
-                        console.log("Error: Internal logic error");
-                        break;
-                    }
-                    case 2: {
-                        console.log("Error: Database error");
-                        break;
-                    }
-                }
-            } else {
-                console.log("Error: " + request.status);
-            }
-        }
+
+    let callback = function (request) {
+        let output = document.getElementById("output_box");
+        output.textContent = ''; // Убираем все элементы потомки, заменяя их на пустую строку
+        const textNode = document.createTextNode("Имя пользователя: " + request.responseText);
+        output.appendChild(textNode)
     };
-    request.send()
+
+    sendRequest("GET", "/api/user/getCurrentName", "", callback, []);
+
 }
 
 function createRoom() {
-    const token = readCookie("token");
-    console.log("Current token: " + token);
-    let request = new XMLHttpRequest();
-    request.open("POST", "/api/room/create", true);
-    request.setRequestHeader("Content-Type", "application/json");
-    request.setRequestHeader("Authorization", "Bearer " + token);
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                let output = document.getElementById("output_box");
-                output.textContent = ''; // Убираем все элементы потомки, заменяя их на пустую строку
-                const textNode = document.createTextNode("Комната создана");
-                output.appendChild(textNode)
-            } else if (request.status === 400) {
-                const data = JSON.parse(request.responseText);
-                switch (parseInt(data["result"])) {
-                    case 5: {
-                        let output = document.getElementById("output_box");
-                        output.textContent = ''; // Убираем все элементы потомки, заменяя их на пустую строку
-                        const textNode = document.createTextNode("Комната уже существует. Создание невозможно");
-                        output.appendChild(textNode)
-                        break;
-                    }
-                    default: {
-                        console.log("Error: internal logic failure. Registration was successful, but login  of the same user failed.")
-                    }
-                }
-            } else if (request.status === 401) {
-                console.log("Non authorised!");
-            } else if (request.status === 500) {
-                const data = JSON.parse(request.responseText);
-                switch (parseInt(data["result"])) {
-                    case 1: {
-                        console.log("Error: Internal logic error");
-                        break;
-                    }
-                    case 2: {
-                        console.log("Error: Database error");
-                        break;
-                    }
-                }
-            } else {
-                console.log("Error: " + request.status);
-            }
-        }
+
+    let callback = function () {
+        let output = document.getElementById("output_box");
+        output.textContent = ''; // Убираем все элементы потомки, заменяя их на пустую строку
+        const textNode = document.createTextNode("Комната создана");
+        output.appendChild(textNode)
     };
     const jsonData = {
         'name': 'testRoom',
         'description': 'This is test room'
     };
-    request.send(JSON.stringify(jsonData))
+    sendRequest("POST", "/api/room/create", JSON.stringify(jsonData), callback, [5]);
 }
 
 function disbandRoom() {
-    const token = readCookie("token");
-    console.log("Current token: " + token);
-    let request = new XMLHttpRequest();
-    request.open("POST", "/api/room/disband", true);
-    request.setRequestHeader("Content-Type", "application/json");
-    request.setRequestHeader("Authorization", "Bearer " + token);
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                let output = document.getElementById("output_box");
-                output.textContent = ''; // Убираем все элементы потомки, заменяя их на пустую строку
-                const textNode = document.createTextNode("Комната расформирована");
-                output.appendChild(textNode)
-            } else if (request.status === 400) {
-                const data = JSON.parse(request.responseText);
-                switch (parseInt(data["result"])) {
-                    case 8: {
-                        let output = document.getElementById("output_box");
-                        output.textContent = ''; // Убираем все элементы потомки, заменяя их на пустую строку
-                        const textNode = document.createTextNode("Комната не существует. Расформирование невозможно");
-                        output.appendChild(textNode)
-                        break;
-                    }
-                    case 9: {
-                        let output = document.getElementById("output_box");
-                        output.textContent = ''; // Убираем все элементы потомки, заменяя их на пустую строку
-                        const textNode = document.createTextNode("Вы не являетесь администратором комнаты.  Расформирование невозможно");
-                        output.appendChild(textNode)
-                        break;
-                    }
-                    default: {
-                        console.log("Error: internal logic failure. Registration was successful, but login  of the same user failed.")
-                    }
-                }
-            } else if (request.status === 401) {
-                console.log("Non authorised!");
-            } else if (request.status === 500) {
-                const data = JSON.parse(request.responseText);
-                switch (parseInt(data["result"])) {
-                    case 1: {
-                        console.log("Error: Internal logic error");
-                        break;
-                    }
-                    case 2: {
-                        console.log("Error: Database error");
-                        break;
-                    }
-                }
-            } else {
-                console.log("Error: " + request.status);
-            }
-        }
+
+    let callback = function () {
+        let output = document.getElementById("output_box");
+        output.textContent = ''; // Убираем все элементы потомки, заменяя их на пустую строку
+        const textNode = document.createTextNode("Комната расформирована");
+        output.appendChild(textNode)
     };
-    request.send()
+
+    sendRequest("POST", "/api/room/disband", "", callback, [8, 9]);
+
 }
 
 let roomId = -1;
 
 function getRooms() {
-    const token = readCookie("token");
-    console.log("Current token: " + token);
-    let request = new XMLHttpRequest();
-    request.open("GET", "/api/room/getInitialList", true);
-    request.setRequestHeader("Content-Type", "application/json");
-    request.setRequestHeader("Authorization", "Bearer " + token);
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                let output = document.getElementById("output_box");
-                output.textContent = ''; // Убираем все элементы потомки, заменяя их на пустую строку
-                const data = JSON.parse(request.responseText);
-                data.forEach(function (item, index) {
-                    const textNode = document.createTextNode("Имя комнаты: " + item.name);
-                    const node = document.createElement("LI");      // Создаем элемент списка <li>
-                    node.appendChild(textNode)
-                    output.appendChild(node)
-                    if (index === 2) {
-                        roomId = item.id;
-                    } else {
-                        roomId = -1;
-                    }
-                });
 
-            } else if (request.status === 401) {
-                console.log("Non authorised!");
-            } else if (request.status === 500) {
-                const data = JSON.parse(request.responseText);
-                switch (parseInt(data["result"])) {
-                    case 1: {
-                        console.log("Error: Internal logic error");
-                        break;
-                    }
-                    case 2: {
-                        console.log("Error: Database error");
-                        break;
-                    }
-                }
+    let callback = function (request) {
+        let output = document.getElementById("output_box");
+        output.textContent = ''; // Убираем все элементы потомки, заменяя их на пустую строку
+        const data = JSON.parse(request.responseText);
+        data.forEach(function (item, index) {
+            const textNode = document.createTextNode("Имя комнаты: " + item.name);
+            const node = document.createElement("LI");      // Создаем элемент списка <li>
+            node.appendChild(textNode)
+            output.appendChild(node)
+            if (index === 2) {
+                roomId = item.id;
             } else {
-                console.log("Error: " + request.status);
+                roomId = -1;
             }
-        }
+        });
     };
-    request.send()
+
+    sendRequest("GET", "/api/room/getInitialList", "", callback, []);
 }
 
 function joinRoom() {
+
     if (roomId < 0) {
-        console.log("Не получен список комнат или в нем нет тестовой комнаты");
+        alert("Не получен список комнат или в нем нет тестовой комнаты.");
         return;
     }
-    const token = readCookie("token");
-    console.log("Current token: " + token);
-    let request = new XMLHttpRequest();
-    request.open("POST", "/api/room/join", true);
-    request.setRequestHeader("Content-Type", "application/json");
-    request.setRequestHeader("Authorization", "Bearer " + token);
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                let output = document.getElementById("output_box");
-                output.textContent = ''; // Убираем все элементы потомки, заменяя их на пустую строку
-                const textNode = document.createTextNode("Вход выполнен успешно");
-                output.appendChild(textNode)
-            } else if (request.status === 400) {
-                const data = JSON.parse(request.responseText);
-                let output = document.getElementById("output_box");
-                output.textContent = ''; // Убираем все элементы потомки, заменяя их на пустую строку
-                switch (parseInt(data["result"])) {
-                    case 4: {
-                        const textNode = document.createTextNode("Неверный пароль для входа");
-                        output.appendChild(textNode)
-                        break;
-                    }
-                    case 5: {
-                        const textNode = document.createTextNode("Пользователь уже находится в другой комнате. Вход невозможен");
-                        output.appendChild(textNode)
-                        break;
-                    }
-                    case 10: {
-                        const textNode = document.createTextNode("Комната не существует. Вход невозможен");
-                        output.appendChild(textNode)
-                        break;
-                    }
-                    case 11: {
-                        const textNode = document.createTextNode("Игра в комнате уже началась. Вход невозможен");
-                        output.appendChild(textNode)
-                        break;
-                    }
-                    default: {
-                        console.log("Error: internal logic failure. Registration was successful, but login  of the same user failed.")
-                    }
-                }
-            } else if (request.status === 401) {
-                console.log("Non authorised!");
-            } else if (request.status === 500) {
-                const data = JSON.parse(request.responseText);
-                switch (parseInt(data["result"])) {
-                    case 1: {
-                        console.log("Error: Internal logic error");
-                        break;
-                    }
-                    case 2: {
-                        console.log("Error: Database error");
-                        break;
-                    }
-                }
-            } else {
-                console.log("Error: " + request.status);
-            }
-        }
+
+    let callback = function () {
+        let output = document.getElementById("output_box");
+        output.textContent = ''; // Убираем все элементы потомки, заменяя их на пустую строку
+        const textNode = document.createTextNode("Вход выполнен успешно");
+        output.appendChild(textNode)
     };
     const jsonData = {
         'id': roomId,
         'password': ''
     };
-    request.send(JSON.stringify(jsonData))
+    sendRequest("POST", "/api/room/join", JSON.stringify(jsonData), callback, [4, 5, 10, 11]);
 }
 
 function leaveRoom() {
-    const token = readCookie("token");
-    console.log("Current token: " + token);
-    let request = new XMLHttpRequest();
-    request.open("POST", "/api/room/leave", true);
-    request.setRequestHeader("Content-Type", "application/json");
-    request.setRequestHeader("Authorization", "Bearer " + token);
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if (request.status === 200) {
-                let output = document.getElementById("output_box");
-                output.textContent = ''; // Убираем все элементы потомки, заменяя их на пустую строку
-                const textNode = document.createTextNode("Выход выполнен успешно");
-                output.appendChild(textNode)
-            } else if (request.status === 400) {
-                const data = JSON.parse(request.responseText);
-                let output = document.getElementById("output_box");
-                output.textContent = ''; // Убираем все элементы потомки, заменяя их на пустую строку
-                switch (parseInt(data["result"])) {
-                    case 8: {
-                        const textNode = document.createTextNode("Вы не находитесь в комнате. Выход невозможен");
-                        output.appendChild(textNode)
-                        break;
-                    }
-                    case 11: {
-                        const textNode = document.createTextNode("Игра в комнате уже началась. Выход невозможен");
-                        output.appendChild(textNode)
-                        break;
-                    }
-                    default: {
-                        console.log("Error: internal logic failure. Registration was successful, but login  of the same user failed.")
-                    }
-                }
-            } else if (request.status === 401) {
-                console.log("Non authorised!");
-            } else if (request.status === 500) {
-                const data = JSON.parse(request.responseText);
-                switch (parseInt(data["result"])) {
-                    case 1: {
-                        console.log("Error: Internal logic error");
-                        break;
-                    }
-                    case 2: {
-                        console.log("Error: Database error");
-                        break;
-                    }
-                }
-            } else {
-                console.log("Error: " + request.status);
-            }
-        }
+
+    let callback = function () {
+        let output = document.getElementById("output_box");
+        output.textContent = ''; // Убираем все элементы потомки, заменяя их на пустую строку
+        const textNode = document.createTextNode("Выход выполнен успешно");
+        output.appendChild(textNode)
     };
-    request.send()
+
+    sendRequest("POST", "/api/room/leave", "", callback, [8, 11]);
 }
 
 function getUsers() {
