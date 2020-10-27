aff575e05e56a292bb4da3c6452d021af937281a
NickGBR <gubarenko.nikita27@gmail.com>
2020-10-27 20:08:12 +0800

    vote result DTO added

--- mafia-web/src/main/java/org/dreamteam/mafia/controller/GameController.java
+++ mafia-web/src/main/java/org/dreamteam/mafia/controller/GameController.java
@@ -25,6 +25,12 @@ public class GameController {
         this.userService = userService;
     }
 
+    @RequestMapping(value = SockConst.REQUEST_GET_VOTE_FOR_USER, method = RequestMethod.GET)
+    public @ResponseBody void voteForUser(@RequestParam String login) throws ClientErrorException {
+        System.out.println(login);
+
+       // gameService.voteCharacter(userService.getCurrentUser());
+    }
 
     @RequestMapping(value = SockConst.REQUEST_GET_ROLE_INFO, method = RequestMethod.GET)
     public @ResponseBody Boolean startGame(@RequestParam String login) throws ClientErrorException {
--- src/main/webapp/js/gameChat.js
+++ src/main/webapp/js/gameChat.js
@@ -275,7 +275,7 @@ function voteForUser(){
         console.log(request.responseText);
     };
     let login = document.getElementById("user_login_value").value;
-    sendRequest("GET", sockConst.REQUEST_GET_ROLE_INFO +"?login=" + login, "", callback, [8]);
+    sendRequest("GET", sockConst.REQUEST_GET_VOTE_FOR_USER +"?login=" + login, "", null, [8]);
 }
 
 function showEndGameScreen(message) {
